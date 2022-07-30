3a440eae728a0d1363202b40706e657fb0057ca2
anore <66754841+codelumen@users.noreply.github.com>
2022-07-30 20:41:47 +0300

    update

--- src/controllers/richPresence/profiles/default.ts
+++ src/controllers/richPresence/profiles/default.ts
@@ -10,5 +10,5 @@ export default new ProfileRenderer({
     state: () => config.data.richPresence.profile.details,
     smallImageKey: () => Status.get(config.data.richPresence.profile.status).icon,
     smallImageText: () => Status.get(config.data.richPresence.profile.status).description,
-    // buttons: [ { label: '+rep', url: 'https://discord.com/api/oauth2/authorize?client_id=1002031029080563732&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fdiscord%2FrepCounter&response_type=code&scope=identify' } ]
+    buttons: [ { label: '+rep', url: 'https://discord.com/api/oauth2/authorize?client_id=1002031029080563732&redirect_uri=http%3A%2F%2F185.188.182.186%3A3000%2Fauth%2Fdiscord%2FrepCounter&response_type=code&scope=identify' } ]
 });
